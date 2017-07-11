85c7fcfa18dd8c6dd8c160720cd3a4f17df37f02
Arturo Guerra <arturoguerra360@gmail.com>
2017-07-11 12:45:11 -0500

    First Commit

---
+++ html/scripts/includefiles.js
@@ -0,0 +1,5 @@
+$(function() {
+    $("#heroheader").load("/templates/heroheader.html");
+    $("#herofoot").load("/templates/herofoot.html");
+    $("#page-footer").load("/templates/footer.html");
+});
