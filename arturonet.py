5312ad7bff49147e76d630515d4abd2e1b5f1d36
ArturoGuerra <arturoguerra360@gmail.com>
2017-07-13 19:32:16 +0000

    HTML CHANGESgit add .

---
+++ arturonet.py
@@ -0,0 +1,14 @@
+from flask import *
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("custom_404.html"), 404
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- website.py
+++
@@ -1,9 +0,0 @@
-from flask import *
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-app.run(host='0.0.0.0', port=5000, debug=True)
