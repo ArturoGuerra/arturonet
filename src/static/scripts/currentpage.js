37bc4a163a9e6218a742bf6e54ea6dd1c85f2786
ArturoGuerra <arturoguerra360@gmail.com>
2017-08-10 21:54:55 +0000

    currentpage.js

--- src/static/scripts/currentpage.js
+++ src/static/scripts/currentpage.js
@@ -4,15 +4,9 @@ var nav = [
     {href:'/social', id:'social', content:'Social'},
     {href:'/projects', id:'projects', content:'Projects'}
     ];
-    args.herofoot_items = [
-    {href:'https://discord.gg/ssl', content:'SSL'},
-    {href:'https://github.com/ArturoGuerra', content:'GitHub'},
-    {href:'https://www.dixionary.com', content:'Dixionary'},
-    {href:'https://inwite.dixionary.com', content:'Dixionary Serwer'}
-    ];
 console.log(currentpage);
 nav.forEach(function(page) {
-    if (nav.href == currentpage) {
+    if (page.href == currentpage) {
         document.getElementById(page.id).classList.add("is-active");
     }
 });
