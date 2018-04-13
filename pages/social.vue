68fc518a3e2a575a8bcf20fa243f738565dcabee
Arturo Guerra <ar2roguerra@gmail.com>
2018-04-13 05:47:45 +0000

    Using grid system for social page

--- pages/social.vue
+++ pages/social.vue
@@ -16,18 +16,18 @@
                   <h1 class="title">My social media</h1>
                   <h2 class="subtitle">Go stalk me in the links below</h2>
                   <hr>
-                  <div class="columns">
-                      <div class="column is-hidden-touch wow slideInRight" >
+                  <div class="social-grid">
+                      <div class="social-dixionary wow slideInRight" >
                           <div><a href="https://inwite.dixionary.com"><h1 class="title"><strong>Dixionary Dixord</strong></h1></a></div>
                           <br>
                           <iframe src="https://discordapp.com/widget?id=203164299458379776&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0"></iframe>
                       </div>
-                      <div class="column is-hidden-touch">
+                      <div class="social-twitter">
                           <div><a href="https://twitter.com/Ar2roGuerra"><h1 class="title"><strong>Twitter</strong></h1></a></div>
                           <br>
                           <a class="twitter-timeline" data-width="350" data-height="500" data-dnt="true" data-theme="dark" href="https://twitter.com/Ar2roGuerra?ref_src=twsrc%5Etfw">Tweets by Ar2roGuerra</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
                       </div>
-                      <div class="column is-hidden-touch wow slideInLeft">
+                      <div class="social-ssl wow slideInLeft">
                           <div><a href="https://discord.gg/ssl"><h1 class="title"><strong>SSL Community</strong></h1></a></div>
                           <br>
                           <iframe src="https://discordapp.com/widget?id=194533269180514305&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0"></iframe>
