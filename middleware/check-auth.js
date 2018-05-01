67e1bdb6ca8b34b670c3657658c1784bfc7c4fa8
Arturo Guerra <ar2roguerra@gmail.com>
2018-05-01 02:26:31 +0000

    middleware things

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -2,12 +2,12 @@ import { isAdmin, getUserFromLocalStorage, getUserFromCookie, getTokenFromLocalS
 
 export default function ({ server, store, req }) {
   // If nuxt generate, pass this middleware
-  // if (server && !req) return
+  if (server && !req) return
   const loggedUser = server ? getUserFromCookie(req) : getUserFromLocalStorage()
   store.commit('SET_USER', loggedUser)
   const token = server ? getTokenFromCookie(req) : getTokenFromLocalStorage()
   store.commit('SET_TOKEN', token)
-  const access_token = server ? getAccessTokenFromCookie(req) : getAccessTokenFromLocalStorage()
+  const access_token = server ? getAccessTokenFromCookie(req) : getAccessTokenFromLocalStorage() // eslint-disable-line camelcase
   store.commit('SET_ACCESS_TOKEN', access_token)
   if (isAdmin(loggedUser)) {
     store.commit('SET_ADMIN', true)
