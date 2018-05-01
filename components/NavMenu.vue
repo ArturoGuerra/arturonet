cb4c2bffbcb93825e1b2136087969ebaf95f2473
Arturo Guerra <ar2roguerra@gmail.com>
2018-05-01 02:50:46 +0000

    middleware things

--- components/NavMenu.vue
+++ components/NavMenu.vue
@@ -1,41 +1,42 @@
 <template>
-    <nav class="navbar has-shadow">
-        <div class="navbar-brand">
-            <nuxt-link to="/" class="navbar-item">
-                <img src="./../assets/img/myface.jpg" class='round-img'>
-                <strong style='color: white;'>&nbsp; Arturo Guerra</strong>
-            </nuxt-link>
-            <div class="navbar-burger burger" id="navtoggle" v-on:click='toggleNav'>
-              <span></span>
-              <span></span>
-              <span></span>
-            </div>
+  <nav class="navbar has-shadow">
+    <div class="navbar-brand">
+        <nuxt-link to="/" class="navbar-item">
+          <img src="./../assets/img/myface.jpg" class='round-img'>
+          <strong style='color: white;'>&nbsp; Arturo Guerra</strong>
+        </nuxt-link>
+        <div class="navbar-burger burger" id="navtoggle" v-on:click='toggleNav'>
+          <span></span>
+          <span></span>
+          <span></span>
         </div>
-        <div class="navbar-menu" id='navmenu'>
-          <div class="navbar-start">
-            <nuxt-link v-for="item in navitems" :id='item.id' :key="item.id" :to="item.href" class="navbar-item is-tab" exact>
+    </div>
+    <div class="navbar-menu" id='navmenu'>
+      <div class="navbar-start">
+        <nuxt-link v-for="item in navitems" :id='item.id' :key="item.id" :to="item.href" class="navbar-item is-tab" exact>
+          <span>{{ item.name }}</span>
+        </nuxt-link>
+      </div>
+      <div class='navbar-end' v-if='isAuthenticated'>
+        <div class='navbar-item has-dropdown is-hoverable'>
+          <span class='navbar-link'>{{ loggedUser.nickname ? loggedUser.nickname : loggedUser.name }}</span>
+          <div class='navbar-dropdown is-boxed'>
+            <nuxt-link v-if='admin' v-for='item in adminnav' :id='item.id' :key='item.id' :to='item.href' class='navbar-item' exact>
+              <span>{{ item.name }}</span>
+            </nuxt-link>
+            <nuxt-link v-for='item in usernav' :id='item.id' :key='item.id' :to='item.href' class='navbar-item' exact>
               <span>{{ item.name }}</span>
             </nuxt-link>
           </div>
-          <div class='navbar-end' v-if='isAuthenticated'>
-            <div class='navbar-item has-dropdown is-hoverable'>
-              <span class='navbar-link'>{{ loggedUser.nickname ? loggedUser.nickname : loggedUser.name }}</span>
-              <div class='navbar-dropdown is-boxed'>
-                <nuxt-link v-if='admin' v-for='item in adminnav' :id='item.id' :key='item.id' :to='item.href' class='navbar-item' exact>
-                  <span>{{ item.name }}</span>
-                </nuxt-link>
-                <nuxt-link v-for='item in usernav' :id='item.id' :key='item.id' :to='item.href' class='navbar-item' exact>
-                  <span>{{ item.name }}</span>
-                </nuxt-link>
-              </div>
-            </div>
-          </div>
-           <div class='navbar-end' v-else>
-              <nuxt-link class='navbar-item is-tab' to='/auth/login'>Login</nuxt-link>
-           </div>
         </div>
-    </nav>
+      </div>
+      <div class='navbar-end' v-else>
+        <nuxt-link class='navbar-item is-tab' to='/auth/login'>Login</nuxt-link>
+      </div>
+    </div>
+  </nav>
 </template>
+
 <script>
 import { mapGetters, mapState } from 'vuex'
 
