898d3af5530822c15cbb18ce58f681329a215749
Arturo Guerra <ar2roguerra@gmail.com>
2021-01-08 16:54:42 -0600

    Upgraded everything

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,15 @@
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
   env: {
     browser: true,
     node: true
   },
-  extends: 'standard',
-  // required to lint *.vue files
+  extends: [
+    '@nuxtjs/eslint-config-typescript',
+    'plugin:nuxt/recommended'
+  ],
   plugins: [
-    'html'
   ],
   // add your custom rules here
-  rules: {},
-  globals: {}
+  rules: {}
 }
